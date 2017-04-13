8bf90ceed3f36e096922bbc99fe422eed19f988d
Andrew Hookom <hookom@gmail.com>
2017-04-13 14:19:40 -0400

    seeding letters based on frequency

--- React/Viewport.js
+++ React/Viewport.js
@@ -19,6 +19,35 @@ import wordlist from '../Dictionary/wordlist.js';
 import commonWordList from '../Dictionary/commonwordlist.js';
 
 const STARTINGCARDS = 10;
+const LETTER_FREQUENCIES = {
+  A: 8.12,
+  B: 1.49,
+  C: 2.71,
+  D: 4.32,
+  E: 12.02,
+  F: 2.30,
+  G: 2.03,
+  H: 5.92,
+  I: 7.31,
+  J: 0.10,
+  K: 0.69,
+  L: 3.98,
+  M: 2.61,
+  N: 6.95,
+  O: 7.68,
+  P: 1.82,
+  Q: 0.11,
+  R: 6.02,
+  S: 6.28,
+  T: 9.10,
+  U: 2.88,
+  V: 1.11,
+  W: 2.09,
+  X: 0.17,
+  Y: 2.11,
+  Z: 0.07
+}
+const LETTER_TABLE = [];
 
 export default class Viewport extends Component {
   constructor(props) {
@@ -43,11 +72,13 @@ export default class Viewport extends Component {
     this.dictionary.bulkAddWords(wordlist);
     let newWord = randomWord();
     store.dispatch(seedWord(newWord));
+    seedLetterTable();
     let currentCards = [];
     while (currentCards.length < STARTINGCARDS) {
       currentCards.push(getNewCard(currentCards, newWord));
     }
     store.dispatch(setPlayerCards(currentCards));
+
   }
 
   componentWillUnmount() {
@@ -177,6 +208,39 @@ export default class Viewport extends Component {
     return this.panResponders[str];
   }
 
+  getPossibleWords() {
+    let possibleWords = [];
+    const word = this.state.word;
+    const currentCards = this.state.playerCards;
+    //try removing each letter
+    for(let i = 0 ; i<word.length;i++){
+      let newWord = word.slice(0,i)+word.slice(i+1);
+      if(this.isValidWord(newWord)){
+        possibleWords.push(newWord)
+      }
+    }
+    //try adding each letter to each position
+    currentCards.forEach(letter=>{
+      for(let i = 0 ; i<=word.length;i++){
+        let newWord = word.slice(0, i) + letter + word.slice(i);
+        if(this.isValidWord(newWord)){
+          possibleWords.push(newWord)
+        }
+      }
+    })
+    //try substituting each letter into each position
+    currentCards.forEach(letter=>{
+      for (let i = 0 ; i < word.length;i++){
+        let newWord = word.slice(0,i) + letter + word.slice(i+1);
+        if (this.isValidWord(newWord)){
+          possibleWords.push(newWord)
+        }
+      }
+    })
+
+    return possibleWords;
+
+  }
 
 }
 
@@ -197,14 +261,27 @@ function setLocationOfCard(str) {
 function getNewCard(currentCards, word) {
   let invalid = currentCards.join('') + word;
 
-  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  let pick = letters.charAt(Math.floor(Math.random() * 26));
+  let pick = LETTER_TABLE[Math.floor(Math.random() * LETTER_TABLE.length)];
 
   while (invalid.includes(pick)) {
-    pick = letters.charAt(Math.floor(Math.random() * 26));
+    pick = LETTER_TABLE[Math.floor(Math.random() * LETTER_TABLE.length)];
   }
+
   return pick;
+
 }
 
 
+function seedLetterTable(){
+  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let letterArr = letters.split('');
+  letterArr.forEach(letter=>{
+    let desiredNumber = Math.round(LETTER_FREQUENCIES[letter]*100);
+    while(desiredNumber){
+      LETTER_TABLE.push(letter);
+      desiredNumber--;
+    }
+  })
+}
